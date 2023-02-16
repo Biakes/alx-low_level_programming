e03c1f85c0160c178af850b33f3eda4664b2792b
Biakes <akinshiloabimbola@gmail.com>
2023-02-16 13:21:47 -0800

    the comma

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (sucess)
+ */
+
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
